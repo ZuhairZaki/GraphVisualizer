8d7d46c146a5f772e8180a975e27d99f6cae48b9
S. M. Zuhair Zaki <zuhairzaki500@gmail.com>
2025-11-12 18:41:57 -0600

    Update kruskal.py
    minor changes

--- kruskal.py
+++ kruskal.py
@@ -26,16 +26,16 @@ class DisjointSet:
         return True
 
 
-def kruskal(vertices, edges):
-    edges = sorted(edges, key=lambda e: e[2])
+def kruskalMST(vertices, edges):
+    edges.sort(key=lambda e: e[2])
 
     ds = DisjointSet(vertices)
     mst_edges = []
-    total_weight = 0
+    mst_weight = 0
 
     for u, v, w in edges:
         if ds.union(u, v):
             mst_edges.append((u, v, w))
-            total_weight += w
+            mst_weight += w
 
-    return mst_edges, total_weight
+    return mst_edges, mst_weight
